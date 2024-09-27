e1979631b2012b512780e4f9cc63e12a005dc8ac
Alipatov326 <155419066+Alipatov326@users.noreply.github.com>
2024-09-27 11:51:26 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,1 +1,14 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+    String info;
+    Scoreboard game = new Scoreboard("Red", "Blue");
+    game.recordPlay(1);
+    info = game.getScore();
+    game.recordPlay(0);
 
+
+    System.out.println(info);
+    }
+}
