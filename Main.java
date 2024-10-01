6e4f2093d3c4aeebfd9fbedd577fb221663bd022
Alipatov326 <155419066+Alipatov326@users.noreply.github.com>
2024-10-01 11:37:40 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,13 +2,25 @@ public class Main
 {
     public static void main(String[] args)
     {
-    String info;
-    Scoreboard game = new Scoreboard("Red", "Blue");
-    game.recordPlay(1);
-    info = game.getScore();
-    game.recordPlay(0);
-
-
-    System.out.println(info);
+        String info;
+        Scoreboard game = new Scoreboard("Red", "Blue");
+        game.recordPlay(1);
+//        info = game.getScore();
+        game.recordPlay(0);
+//        info = game.getScore();
+        game.recordPlay(3);
+//        info = game.getScore();
+        game.recordPlay(1);
+        game.recordPlay(0);
+//        info = game.getScore();
+        game.recordPlay(0);
+        game.recordPlay(4);
+        game.recordPlay(0);
+//        info = game.getScore();
+        Scoreboard match = new Scoreboard("Lions", "Tigers");
+        info = match.getScore();
+        System.out.println(info);
+        info = game.getScore();
+        System.out.println(info);
     }
 }
